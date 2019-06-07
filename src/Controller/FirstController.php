f3922ab87a406d9b2e0af0a2c597893e79de3d5c
pacman_n <sparfenuk13@gmail.com>
2019-06-07 23:10:58 +0300

    db and some tests

--- src/Controller/FirstController.php
+++ src/Controller/FirstController.php
@@ -3,17 +3,33 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
 
 class FirstController extends AbstractController
 {
     /**
-     * @Route("/first", name="first")
+     * @Route("/", name="first")
      */
     public function index()
     {
         return $this->render('first/index.html.twig', [
             'controller_name' => 'FirstController',
         ]);
+//        return new Response("<html><body>dsadsadsdasd</body></html>",Response::HTTP_OK);
     }
+
+    /**
+     * @Route("/welcome", name="first_page")
+     */
+    public function hello(Request $request){
+        $name = $request->query->get('name', 'Anonymous');
+
+        return $this->render('first_page.html.twig',[
+            'name' => $name,
+
+        ]);
+    }
+
 }
